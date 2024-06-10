f933657bbe6c83fe0d3a0dfe9567fd1d6b7f3e98
Anshu Geetanjali <ageetanjali308@gmail.com>
2024-06-11 00:39:30 +0530

    Add HealthCheck controller to monitor the status and availability of the application

---
+++ src/main/java/com/anshu/journalApp/controller/HealthCheck.java
@@ -0,0 +1,14 @@
+package com.anshu.journalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/healthCheck")
+    public String healthCheck() {
+        return "OK";
+    }
+
+}
