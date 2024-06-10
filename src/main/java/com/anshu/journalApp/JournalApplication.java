2ed210743242d13b29254388f1c4eeb86ad005dd
Anshu Geetanjali <ageetanjali308@gmail.com>
2024-06-10 23:48:26 +0530

    Add main application class to run the Spring Boot application

---
+++ src/main/java/com/anshu/journalApp/JournalApplication.java
@@ -0,0 +1,14 @@
+package com.anshu.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalApplication.class, args);
+	}
+
+}
+
