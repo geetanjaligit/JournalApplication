41d6ff2bd73e67f482486008fb69efc4f844c250
Anshu Geetanjali <ageetanjali308@gmail.com>
2024-07-05 19:48:50 +0530

    Use Lombok to simplify entity class and reduce codebase.

--- src/main/java/com/anshu/journalApp/entity/JournalEntry.java
+++ src/main/java/com/anshu/journalApp/entity/JournalEntry.java
@@ -7,9 +7,15 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 @Document(collection="Journal_entries")
 //(collection = "Journal_entries")//the @Document annotation is used to configure the mapping 
 //between the JournalEntry class and the MongoDB collection.
+
+@Data
 public class JournalEntry {
 
     @Id
@@ -18,70 +24,5 @@ public class JournalEntry {
     private String content;
     private LocalDateTime date;
 
-
-    /**
-     * @return long return the id
-     */
-    public ObjectId getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    /**
-     * @return String return the title
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * @param title the title to set
-     */
-    // public void settitlee(String title) {
-    //     this.title = title;
-    // }
-
-    /**
-     * @return String return the content
-     */
-    public String getContent() {
-        return content;
-    }
-
-    /**
-     * @param content the content to set
-     */
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-
-    /**
-     * @param title the title to set
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-
-    /**
-     * @return Date return the date
-     */
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    /**
-     * @param date the date to set
-     */
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 
 }
