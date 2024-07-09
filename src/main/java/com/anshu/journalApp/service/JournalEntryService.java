76a5b45ad2361f074692e4337fee812d6bfc8a6a
Anshu Geetanjali <ageetanjali308@gmail.com>
2024-07-09 11:13:21 +0530

    Implement JournalEntryService with user-specific CRUD operations

--- src/main/java/com/anshu/journalApp/service/JournalEntryService.java
+++ src/main/java/com/anshu/journalApp/service/JournalEntryService.java
@@ -1,5 +1,6 @@
 package com.anshu.journalApp.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.anshu.journalApp.entity.JournalEntry;
+import com.anshu.journalApp.entity.User;
 import com.anshu.journalApp.repository.JournalEntryRepository;
 
 @Component
@@ -17,10 +19,23 @@ public class JournalEntryService {
 
     @Autowired
     private JournalEntryRepository repository;
+
+    @Autowired
+    private UserService userService;
      
+    public void saveEntry(JournalEntry entry ,String username) {
+        User user=userService.findByUsername(username);
+        entry.setDate(LocalDateTime.now());
+        JournalEntry saved=repository.save(entry);
+        user.getJournalEntries().add(saved);
+        userService.saveEntry(user);
+    }
+
     public void saveEntry(JournalEntry entry) {
+        entry.setDate(LocalDateTime.now());
         repository.save(entry);
     }
+    
 
     public List<JournalEntry> getAllEntries() {
         return repository.findAll();
@@ -30,8 +45,12 @@ public class JournalEntryService {
         return repository.findById(id);
     }
 
-    public void deleteEntryById(ObjectId id) {
+    public void deleteEntryById(ObjectId id,String username) {
+        User user=userService.findByUsername(username); 
+        user.getJournalEntries().removeIf(x->x.getId().equals(id));
+        userService.saveEntry(user);
         repository.deleteById(id);
     }
 
-}
+} 
+ 
